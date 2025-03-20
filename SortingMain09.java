cd3810c9a48de409e112b64a2c09dcf9ce0aa97f
unknown <bima.rafi60@sma.belajar.id>
2025-03-20 08:53:23 +0700

    modif 1

--- SortingMain09.java
+++ SortingMain09.java
@@ -2,14 +2,23 @@ public class SortingMain09 {
     public static void main(String[] args) {
 
         int a[] = {20, 10, 2, 7, 12};
+        int b[] = {30, 20, 2, 8, 14};
     
         Sorting09 dataurut1 = new Sorting09(a, a.length);
+        Sorting09 dataurut2 = new Sorting09(b, b.length);
 
         System.out.println("Data Awal 1");
         dataurut1.tampil();
         dataurut1.bubbleSort();
         System.out.println("Data sudah diurutkan dengan BUBBLE SORT (ASC)");
         dataurut1.tampil();
+        System.out.println();
+
+        System.out.println("Data Awal 2");
+        dataurut2.tampil();
+        dataurut2.SelectionSort();
+        System.out.println("Data sudah diurutkan dengan SELECTION SORT (ASC)");
+        dataurut2.tampil();
     }
 
     
